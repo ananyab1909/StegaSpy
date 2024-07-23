9685a634974890ac371a1f23721c8b143421fd55
ananyab1909 <bananya1909@gmail.com>
2024-07-24 00:20:33 +0530

    html updated

---
+++ app.js
@@ -0,0 +1,32 @@
+const button1 = document.getElementsByClassName('button')[0];
+const button2 = document.getElementsByClassName('button')[1];
+const button3 = document.getElementsByClassName('button')[2];
+const button4 = document.getElementsByClassName('button')[3];
+
+const div1 = document.getElementsByClassName('intro')[0];
+const div2 = document.getElementsByClassName('encoding')[0];
+
+button1.addEventListener('click',()=>{
+    div1.style.display ='none';
+    div2.style.display ='none';
+    home.style.display ='block';
+})
+
+button2.addEventListener('click',()=>{
+    div1.style.display ='none';
+    div2.style.display ='none';
+    home.style.display ='block';
+})
+
+button3.addEventListener('click',()=>{
+    div1.style.display ='none';
+    div2.style.display ='none';
+    home.style.display ='block';
+})
+
+button4.addEventListener('click',()=>{
+    div1.style.display ='none';
+    div2.style.display ='none';
+    home.style.display ='block';
+})
+
