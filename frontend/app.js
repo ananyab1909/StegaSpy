70cfc3c6b6936e936b65ed668bde68e55e045201
ananyab1909 <bananya1909@gmail.com>
2024-07-25 00:29:35 +0530

    frontend updated

--- frontend/app.js
+++ frontend/app.js
@@ -3,30 +3,66 @@ const button2 = document.getElementsByClassName('button')[1];
 const button3 = document.getElementsByClassName('button')[2];
 const button4 = document.getElementsByClassName('button')[3];
 
+const img = document.querySelector('.image-hide');
+img.style.display = 'none';
+
+const audio = document.querySelector('.audio-hide');
+audio.style.display = 'none';
+
+const vid = document.querySelector('.video-hide');
+vid.style.display = 'none';
+
+const text = document.querySelector('.text-hide');
+text.style.display = 'none';
+
 const div1 = document.getElementsByClassName('intro')[0];
 const div2 = document.getElementsByClassName('encoding')[0];
 
 button1.addEventListener('click',()=>{
     div1.style.display ='none';
     div2.style.display ='none';
-    home.style.display ='block';
+    img.style.display = 'block';
 })
 
 button2.addEventListener('click',()=>{
     div1.style.display ='none';
     div2.style.display ='none';
-    home.style.display ='block';
+    audio.style.display = 'block';
 })
 
 button3.addEventListener('click',()=>{
     div1.style.display ='none';
     div2.style.display ='none';
-    home.style.display ='block';
+    vid.style.display = 'block';
 })
 
 button4.addEventListener('click',()=>{
     div1.style.display ='none';
     div2.style.display ='none';
-    home.style.display ='block';
+    text.style.display = 'block';
 })
+
+const dropArea = document.getElementById("drop-area1");
+const inputFile = document.getElementById("input-file1");
+const imageView = document.getElementById("img-view");
+
+inputFile.addEventListener("change" , uploadImage);
+
+function uploadImage() {
+    let imgLink = URL.createObjectURL(inputFile.files[0]);
+    imageView.style.backgroundImage = `url('${imgLink}')`;
+    imageView.textContent = "";
+    imageView.style.border = 0;
+}
+
+dropArea.addEventListener("dragover",function(e){
+    e.preventDefault();
+});
+
+dropArea.addEventListener("drop" , function(e){
+    e.preventDefault();
+    inputFile.files = e.dataTransfer.files;
+});
+
+
 
